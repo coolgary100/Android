161f9a3b83cfc175d54b27b0555e270409a0d502
coolgary100 <bowengary@hotmail.com>
2016-07-07 17:25:43 -0700

    And another one

---
+++ TransitionsandAnimations2/app/src/main/java/com/gary/transitionsandanimations/MainActivity.java
@@ -0,0 +1,91 @@
+package com.gary.transitionsandanimations;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.transition.TransitionManager; //Lets u have smooth transition
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewGroup buckysLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buckysLayout = (ViewGroup) findViewById(R.id.buckysLayout);
+
+        buckysLayout.setOnTouchListener(
+                new RelativeLayout.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        moveButton();
+                        return true;
+                    }
+                }
+        );
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void moveButton() {
+        View buckysButton = findViewById(R.id.buckysButton);
+
+        TransitionManager.beginDelayedTransition(buckysLayout); //Let's you have a transition instead of instant
+
+        //Change the position of the button
+        RelativeLayout .LayoutParams positionRules = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        positionRules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        positionRules.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        buckysButton.setLayoutParams(positionRules);
+        //Change the size of the button
+        ViewGroup.LayoutParams sizeRules = buckysButton.getLayoutParams();
+        sizeRules.width = 450;
+        sizeRules.height = 300;
+        buckysButton.setLayoutParams(sizeRules);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
